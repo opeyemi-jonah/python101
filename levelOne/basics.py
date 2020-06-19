7a9e5dbd49071ee1209b47bd4de83cba6270fac6
opeyemi-jonah <gavrieljonah@gmail.com>
2020-06-19 10:05:31 -0600

    created an array

---
+++ levelOne/basics.py
@@ -0,0 +1,14 @@
+#Print format
+
+who_are_you = "My name is {} and I am {}".format('OPeyemi','Fullstack Web Developer')
+ 
+# print(who_are_you)
+
+#sort a list
+
+#multi dimensional array
+
+mylist = [[[1,2,3],[5,6,7],[8,9,-1]],[[12,2,93],[5,60,7],[10,9,-1]]]
+print(mylist)
+
+print([row[2][0] for row in mylist])
