2df75371559d1302538604f6cdd94c178002a7b5
opeyemi-jonah <gavrieljonah@gmail.com>
2020-07-11 09:02:17 -0600

    exercise solutions

--- levelOne/exercise6.py
+++ levelOne/exercise6.py
@@ -73,5 +73,5 @@ name = "Sammy"
 # Use print formatting to print the following string:
 "Hello my dog's name is Sammy and he is 4 years old"
 
-print("Hello my dog's name is {name} and he is {age} years old".format(age = 4,
-name = "Sammy"))
+print("Hello my dog's name is {y} and he is {x} years old".format(x=age,
+y=name))
